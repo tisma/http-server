158b3d9402bd2b161e81613de938e493321a0572
Miroslav Tisma <tisma.etf@gmail.com>
2023-05-12 16:51:50 +0200

    Add main executable, fix mock data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod server;
+
+#[path = "./controllers/mock_controller.rs"]
+mod mock_controller;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = server::start();
+
+    println!("Server started on port {}", server::port());
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        let http_fields = server::request(&stream);
+
+        mock_controller::mock(stream, http_fields);
+    }
 }
